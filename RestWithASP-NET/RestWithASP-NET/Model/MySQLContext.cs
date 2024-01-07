4c3a0ce8dcb9e349171a7c79cf61974ad5b8921b
marliseborba <borba.marlise@gmail.com>
2024-01-07 15:18:35 -0300

    Create DbContext

---
+++ RestWithASP-NET/RestWithASP-NET/Model/MySQLContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace RestWithASP_NET.Model
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+
+        }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
