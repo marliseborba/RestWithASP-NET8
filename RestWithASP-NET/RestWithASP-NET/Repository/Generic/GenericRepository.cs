0be6edc22fa14052d9446578c72eef8063923473
marliseborba <borba.marlise@gmail.com>
2024-01-24 23:26:00 -0300

    Implementing generic repository structure

---
+++ RestWithASP-NET/RestWithASP-NET/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using RestWithASP_NET.Model.Base;
+
+namespace RestWithASP_NET.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public T Create(T T)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindByID(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T T)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RestWithASP-NET/RestWithASP-NET/Repository/Generic/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASP_NET.Model;
+using RestWithASP_NET.Model.Base;
+
+namespace RestWithASP_NET.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T T);
+        T FindByID(long id);
+        List<T> FindAll();
+        T Update(T T);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
