35bb5d13197db7e0db99ea239d2722eff40704ea
marliseborba <borba.marlise@gmail.com>
2024-02-01 05:29:45 -0300

    Annotations for rename and ignore

--- RestWithASP-NET/RestWithASP-NET/Data/VO/PersonVO.cs
+++ RestWithASP-NET/RestWithASP-NET/Data/VO/PersonVO.cs
@@ -1,15 +1,17 @@
 ﻿using RestWithASP_NET.Model.Base;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace RestWithASP_NET.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
-
+        [JsonIgnore]
         public string Address { get; set; }
 
         public string Gender { get; set; }
