217aecc105818e25df79e0e44c2028ac1ec30597
marliseborba <borba.marlise@gmail.com>
2024-02-04 09:10:31 -0300

    Implementing HyperMediaLink class

---
+++ RestWithASP-NET/RestWithASP-NET/Hypermedia/HyperMediaLink.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace RestWithASP_NET.Hypermedia
+{
+    public class HyperMediaLink
+    {
+        public string Rel { get; set; }
+        // Atributo interno apenas para tratar a barra que é substituída por %2F
+        private string href;
+        public string Href {
+            get
+            {
+                object _lock = new object();
+                lock (_lock) 
+                {
+                    StringBuilder sb = new StringBuilder(href);
+                    return sb.Replace("%2F", "/").ToString();
+                }
+            }
+            set{
+                href = value;
+            }
+        }
+        public string Type { get; set; }
+        public string Action { get; set; }
+    }
+}
