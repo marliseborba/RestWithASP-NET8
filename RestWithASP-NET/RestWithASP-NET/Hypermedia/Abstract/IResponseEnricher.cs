8f7163bf9781d57c0975f63e852ae075203ce0aa
marliseborba <borba.marlise@gmail.com>
2024-02-04 05:10:02 -0300

    Creating Constants and Interfaces

---
+++ RestWithASP-NET/RestWithASP-NET/Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RestWithASP_NET.Hypermedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutedContext context);
+        Task Enrich(ResultExecutedContext context);
+    }
+}
---
+++ RestWithASP-NET/RestWithASP-NET/Hypermedia/Abstract/ISupportHyperMedia.cs
@@ -0,0 +1,7 @@
+﻿namespace RestWithASP_NET.Hypermedia.Abstract
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
